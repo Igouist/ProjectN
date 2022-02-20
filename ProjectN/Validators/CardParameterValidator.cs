4eb6554a680ff8bcc238d34d7af57971e544d7cb
Igouist <Ursaliker@gmail.com>
2022-02-20 13:22:13 +0800

    Chore: Add FluentValidation

---
+++ ProjectN/Validators/CardParameterValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using ProjectN.Parameter;
+
+namespace ProjectN.Validators
+{
+    /// <summary>
+    /// Card Parameter 的驗證器
+    /// </summary>
+    public class CardParameterValidator : AbstractValidator<CardParameter>
+    {
+        /// <summary>
+        /// 驗證器的建構式: 在這裡註冊我們要驗證的規則
+        /// </summary>
+        public CardParameterValidator()
+        {
+
+        }
+    }
+}
